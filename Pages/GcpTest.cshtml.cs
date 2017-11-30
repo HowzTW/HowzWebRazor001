de6bb0349b23ea08bdf730ec25683cd91a122235
Howz <how.cht@gmail.com>
2017-11-30 15:49:34 +0800

    測試Google Cloud Datastore
    測試Google Cloud Datastore

--- Pages/GcpTest.cshtml.cs
+++ Pages/GcpTest.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Google.Cloud.Datastore.V1;
 
 namespace HowzWebRazor001.Pages
 {
@@ -13,7 +14,38 @@ namespace HowzWebRazor001.Pages
 
         public void OnGet()
         {
-            Message = "Your Goole Cloud Platform Test page. (測試)";
+            // Your Google Cloud Platform project ID
+            string projectId = "howzgcp004";
+
+            // Instantiates a client
+            DatastoreDb db = DatastoreDb.Create(projectId);
+
+            // The kind for the new entity
+            string kind = "Task";
+            // The name/ID for the new entity
+            string name = "sampletask1";
+            KeyFactory keyFactory = db.CreateKeyFactory(kind);
+            // The Cloud Datastore key for the new entity
+            Key key = keyFactory.CreateKey(name);
+
+            var task = new Entity
+            {
+                Key = key,
+                ["description"] = "Buy milk"
+            };
+            using (DatastoreTransaction transaction = db.BeginTransaction())
+            {
+                // Saves the task
+                transaction.Upsert(task);
+                transaction.Commit();
+
+                Console.WriteLine($"Saved {task.Key.Path[0].Name}: {(string)task["description"]}");
+            }
+
+
+
+
+            Message = "Your Goole Cloud Platform Test page. (測試task)";
         }
     }
 }
