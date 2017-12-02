749d019c2c342c1d3b7257a2f5ecb1f238a7691d
Howz <how.cht@gmail.com>
2017-12-02 20:29:34 +0800

    新增員工資料維護
    新增員工資料維護

--- Pages/Employee/Index.cshtml.cs
+++ Pages/Employee/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Google.Cloud.Datastore.V1;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,20 @@ namespace HowzWebRazor001.Pages.Employee
 {
     public class IndexModel : PageModel
     {
+        public string Message { get; set; }
+        public List<Entity> Employees { get; set; }
+
         public void OnGet()
         {
+            DatastoreDb db = GoogleCloudDatastore.CreateDb();
+
+            // 查詢現有貝工
+            Query query = new Query("Employee");
+            var allEmployee = db.RunQueryLazily(query);
+            int count = allEmployee.Count();
+            Employees = allEmployee.ToList();
+
+            Message = "共有 " + count + " 筆員工資料";
         }
     }
 }
